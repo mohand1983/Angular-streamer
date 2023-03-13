3af03112763106fb222d6dfc11b854240daca8bc
boudi1983 <boudi.mohand@gmail.com>
2023-03-13 14:21:02 +0100

    Add routing, css and different *ngif et *ngfor

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  /**
+   * Tiles to display in the html template 
+   */
+  public tiles: Array<any>=[]
+  /**
+   * Specify if a "user" is admin or nt (default true)
+   */
+  public isAdmin:boolean=false
 
   constructor() { }
 
   ngOnInit(): void {
+    this.tiles.push({
+      title:'Parameters',
+      summary:'Parameters management',
+      action:['dashboard']
+    },
+    {
+      title:'Students',
+      summary:'Add, Remove, View',
+      action:['dashboard']
+    },
+    {
+      title:'Courses',
+      summary:'Parameters management',
+      action:['dashboard']
+    })
+  }
+  public onClick(object:any): void{
+    console.log(`A button was clicked :${object.title}`)
   }
 
 }
