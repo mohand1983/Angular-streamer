ef284e4d5880571505b44b1667b7ec1c379b67ac
boudi1983 <boudi.mohand@gmail.com>
2023-03-13 12:13:42 +0100

    init app angular streamer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(AppRoutingModule.routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  public static readonly routes:Routes = [
+    {
+      path: '', // Mean : http://localhost:4200
+      redirectTo: 'dashboard', // Redirection to another Route object
+      pathMatch: 'full' //Mean Anguler read the whole URI instead of first matching occ
+    },
+    {
+      path: 'dashboard',
+      component: DashboardComponent
+    },
+    {
+      path: '**',
+      redirectTo: 'dashboard',
+      pathMatch: 'full'
+    }
+  ]
+}
