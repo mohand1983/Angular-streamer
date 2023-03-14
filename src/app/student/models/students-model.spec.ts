da4a2765798be432315fd9d83b28d597ae05da34
boudi1983 <boudi.mohand@gmail.com>
2023-03-14 10:51:15 +0100

    model and test model

--- src/app/student/models/students-model.spec.ts
+++ src/app/student/models/students-model.spec.ts
@@ -4,4 +4,14 @@ describe('StudentsModel', () => {
   it('should create an instance', () => {
     expect(new StudentsModel()).toBeTruthy();
   });
+  it(`Should have 'Aubert' as lastname`, ()=>{
+    const student: StudentsModel=new StudentsModel()
+    //fill student object
+    student.lastName='Aubert'
+    student.email='jean_luc.aubert@aelion.fr'
+    student.login='jlaubert'
+    student.password='toto'
+    expect(student.lastName).toBe('Aubert')
+
+  })
 });
