9df93f0ce4437ed6b9c62bf0ca4d226b05ad9d11
boudi1983 <boudi.mohand@gmail.com>
2023-03-17 15:23:30 +0100

    Revert "Add our pipe for whrite initial lastName and firstName"
    This reverts commit 49a0922b022b2435cb8dcdf3529004a9daecfd89.

--- src/app/student/pipes/initials.pipe.ts
+++ src/app/student/pipes/initials.pipe.ts
@@ -1,13 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { IStudent } from '../interfaces/i-student';
 
 @Pipe({
   name: 'initials'
 })
 export class InitialsPipe implements PipeTransform {
 
-  transform(value: IStudent, ...args: unknown[]): unknown {
-    return value.firstName!.charAt(0) + value.lastName.charAt(0);
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
   }
 
 }
